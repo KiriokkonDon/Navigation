3ac813a78dd151af7739d87525753e0504c7f16f
KiriokkonDon <akane.vip@mail.ru>
2024-06-11 00:27:56 +1100

    Button + Fragments + xml

---
+++ app/src/main/java/com/example/navigation/fragments/MainFragment.kt
@@ -0,0 +1,31 @@
+package com.example.navigation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.navigation.R
+
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val bF1 = view.findViewById<Button>(R.id.buttonF1)
+        val bF2 = view.findViewById<Button>(R.id.buttonF2)
+        bF1.setOnClickListener {  }
+        bF2.setOnClickListener {  }
+    }
+
+}
