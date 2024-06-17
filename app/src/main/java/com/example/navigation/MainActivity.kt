113bf9e6db78887d145a134ace061e266c921f7e
KiriokkonDon <akane.vip@mail.ru>
2024-06-17 23:49:26 +1100

    Fix

--- app/src/main/java/com/example/navigation/MainActivity.kt
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -7,8 +7,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,8 +30,14 @@ class MainActivity : AppCompatActivity() {
         animDrawable.setEnterFadeDuration(10)
         animDrawable.setExitFadeDuration(5000)
         animDrawable.start()
-
+        
+        lifecycleScope.launch {
+            performLongOperation()
+        }
     }
 
-
+    private suspend fun performLongOperation() {
+        withContext(Dispatchers.IO) {
+        }
+    }
 }
